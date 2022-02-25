4fdeeba22ca8431de921b698ac7207930ea02eb8
EsoFine20 <ethan.s.fine@gmail.com>
2022-02-25 15:36:14 -0500

    Level select button routes properly

--- app/src/main/java/com/example/project5236/HomeFragment.java
+++ app/src/main/java/com/example/project5236/HomeFragment.java
@@ -29,7 +29,7 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.playButton.setOnClickListener(new View.OnClickListener() {
+        binding.levelSelectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(HomeFragment.this)
