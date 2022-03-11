9da26b1def1d2bdefdfe9ac35c56e2f135165094
gzimpfer90 <zimpfer.25@osu.edu>
2022-03-11 18:51:11 -0500

    Adds update and delete

--- app/src/main/java/com/example/project5236/PlayCRUDFragment.java
+++ app/src/main/java/com/example/project5236/PlayCRUDFragment.java
@@ -1,7 +1,6 @@
 package com.example.project5236;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,19 +8,13 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.project5236.databinding.FragmentPlayCrudBinding;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
 
 public class PlayCRUDFragment extends Fragment {
 
@@ -29,6 +22,11 @@ public class PlayCRUDFragment extends Fragment {
     private String nextName;
     private int nextLevelVal;
     private String readList;
+    private int updateStars = 0;
+    private int updateLevelVal;
+    private String updateLevelName;
+    private int deleteLevelVal;
+    private String deleteLevelName;
     private FragmentPlayCrudBinding binding;
     FirebaseDatabase rootNode;
     DatabaseReference levelsReference;
@@ -81,6 +79,56 @@ public class PlayCRUDFragment extends Fragment {
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         readList = snapshot.getValue().toString();
                         Log.d(TAG, readList);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
+        binding.buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                levelsReference = rootNode.getReference("Levels");
+                nextReference = rootNode.getReference("NextLevel");
+                nextReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        updateStars += 1;
+                        if (updateStars == 4) {
+                            updateStars = 0;
+                        }
+                        updateLevelVal = Integer.parseInt(snapshot.getValue().toString()) - 1;
+                        updateLevelName = "Level" + updateLevelVal;
+                        LevelHelperClass updateHelper = new LevelHelperClass(0, updateStars);
+                        levelsReference.child(updateLevelName).setValue(updateHelper);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
+        binding.buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                levelsReference = rootNode.getReference("Levels");
+                nextReference = rootNode.getReference("NextLevel");
+                nextReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        deleteLevelVal = Integer.parseInt(snapshot.getValue().toString()) - 1;
+                        deleteLevelName = "Level" + deleteLevelVal;
+                        levelsReference.child(deleteLevelName).removeValue();
+                        nextReference.setValue(deleteLevelVal);
                     }
 
                     @Override
